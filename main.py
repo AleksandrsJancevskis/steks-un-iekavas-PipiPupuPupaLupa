a75b392b9ed181fec7796378e4f930deac68c1ef
AleksandrsJancevskis <125644026+AleksandrsJancevskis@users.noreply.github.com>
2023-02-19 15:30:34 +0000

    commit_10

--- main.py
+++ main.py
@@ -2,36 +2,33 @@
 #Aleksandrs Jančevskis 221RDB340
 from collections import namedtuple
 
-Bracket = namedtuple("Bracket", ["char", "position"])
+Bracket = namedtuple("Bracket", ["char", "position"])   
 
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
-
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket
-            opening_brackets_stack.append((next, i + 1))
+    for i, next_char in enumerate(text):
+        if next_char in "([{": 
+            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next_char, i + 1)) 
 
-        if next in ")]}":
-            # Process closing bracket
-            if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack[-1][0], next):
-                return i + 1
-            opening_brackets_stack.pop()
+        if next_char in ")]}":
+            # Process closing bracket, write your code here
+            if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack[-1].char, next_char):
+                print(i+1)
+                return
+            opening_brackets_stack.pop() 
 
-    if len(opening_brackets_stack) == 0:
-        return "Success"
+    if len(opening_brackets_stack) != 0:
+        print(opening_brackets_stack[-1].position)
     else:
-        return opening_brackets_stack[0][1]
+        print("Success")
 
-
-def main():
+def main(): 
     text = input()
-    mismatch = find_mismatch(text)
-    print(mismatch)
-
+    find_mismatch(text)
 
 if __name__ == "__main__":
     main()
