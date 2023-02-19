d16cc69f5ccf9008980f2be5a07b5cc95dfb303c
AleksandrsJancevskis <125644026+AleksandrsJancevskis@users.noreply.github.com>
2023-02-19 15:16:42 +0000

    commit_8

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
+    for I, next in enumerate(text):
         if next in "([{": 
             # Process opening bracket
-            opening_brackets_stack.append(Bracket(next, i + 1))
+            opening_brackets_stack.append(Bracket(next, I + 1))
   
         if next in ")]}":
             # Process closing bracket
             if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack[-1].char, next):
-                return i + 1
+                return I + 1
             opening_brackets_stack.pop()
 
     if len(opening_brackets_stack) != 0:
