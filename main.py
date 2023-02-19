802bc8500971357807b00b560a504c9eef63ad62
AleksandrsJancevskis <125644026+AleksandrsJancevskis@users.noreply.github.com>
2023-02-19 15:14:08 +0000

    commit_7

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 # python3
 #Aleksandrs Jančevskis 221RDB340
-from collections import namedtuple 
+from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
- 
-def find_mismatch(text): 
-    opening_brackets_stack = []
-    for i, next in enumerate(text): 
-        if next in "([{":
-            # Process opening bracket
-            opening_brackets_stack.append(Bracket(next, i + 1)) 
 
+
+def find_mismatch(text):
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{": 
+            # Process opening bracket
+            opening_brackets_stack.append(Bracket(next, i + 1))
+  
         if next in ")]}":
             # Process closing bracket
             if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack[-1].char, next):
-                return i + 1 
+                return i + 1
             opening_brackets_stack.pop()
 
     if len(opening_brackets_stack) != 0:
-        return opening_brackets_stack[-1].position 
-
+        return opening_brackets_stack[-1].position
+  
     return "Success"
 
 def main():
-    text = input()
+    text = input() 
+    if len(text) == 0:
+        print("Success")
+        return
+ 
     mismatch = find_mismatch(text)
     # Printing answer
-    if mismatch == "Success" or len(mismatch) == 0:
+    if mismatch == "Success":
         print("Success")
     else:
-        print(mismatch)
+        print(mismatch) 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
