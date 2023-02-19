47e254447f65c5446ea649e01697aaf0324aa673
AleksandrsJancevskis <125644026+AleksandrsJancevskis@users.noreply.github.com>
2023-02-19 14:54:51 +0000

    commit_6

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 # python3
 #Aleksandrs Jančevskis 221RDB340
-
-from collections import namedtuple
+from collections import namedtuple 
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
-
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
-
-
-def find_mismatch(text):
+ 
+def find_mismatch(text): 
     opening_brackets_stack = []
-    for i, next in enumerate(text):
+    for i, next in enumerate(text): 
         if next in "([{":
             # Process opening bracket
-            opening_brackets_stack.append(Bracket(next, i + 1))
+            opening_brackets_stack.append(Bracket(next, i + 1)) 
 
         if next in ")]}":
             # Process closing bracket
             if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack[-1].char, next):
-                return i + 1
+                return i + 1 
             opening_brackets_stack.pop()
 
     if len(opening_brackets_stack) != 0:
-        return opening_brackets_stack[-1].position
+        return opening_brackets_stack[-1].position 
 
     return "Success"
-
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer
-    print(mismatch if isinstance(mismatch, str) else str(mismatch))
-
+    if mismatch == "Success" or len(mismatch) == 0:
+        print("Success")
+    else:
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
